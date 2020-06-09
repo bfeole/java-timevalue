00d45e967b8b349f91659f0e5ef3c1a32e17f19e
Zurglots <bfeole@gmail.com>
2020-06-09 19:03:20 -0400

    Inital spring boot maven set up

---
+++ src/main/java/local/bfeole/timevalue/TimeValueApplication.java
@@ -0,0 +1,15 @@
+package local.bfeole.timevalue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeValueApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TimeValueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/local/bfeole/timevalue/TimeValueApplicationTests.java
@@ -0,0 +1,15 @@
+package local.bfeole.timevalue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeValueApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
